17c807078062136d60a878b90c080a481e618365
NicBasansky <n.basansky@gmail.com>
2021-08-22 11:50:19 -0700

    Added standard assets to the project. Player controller and camera zoom functionality set up

---
+++ Assets/Scripts/ZoomLook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomLook : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+    [SerializeField] float zoomInFov = 37f;
+
+    [SerializeField] float lineTraceRange = 3f;
+
+    float initialFOV;
+
+    void Start()
+    {
+        initialFOV = camera.fieldOfView;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Zoom") || Input.GetMouseButtonDown(1))
+        {
+            ZoomIn();          
+            ProcessRaycast();
+        }
+        else if (Input.GetButtonUp("Zoom") || Input.GetMouseButtonUp(1))
+        {
+            ZoomOut();
+        }
+    }
+
+    void ZoomIn() // todo smooth camera
+    {
+        camera.fieldOfView = zoomInFov;
+    }
+
+    void ProcessRaycast()
+    {   
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, lineTraceRange))
+        {
+            var interactableObj = hit.collider.GetComponent<IInteractable>();
+            if(interactableObj != null)
+            {
+                interactableObj.OnInteract();
+            }          
+        }
+        Debug.DrawLine(camera.transform.position, transform.position + (camera.transform.forward * lineTraceRange), Color.red, 5f);
+    }
+
+    void ZoomOut()
+    {
+        camera.fieldOfView = initialFOV;
+
+    }
+   
+}
